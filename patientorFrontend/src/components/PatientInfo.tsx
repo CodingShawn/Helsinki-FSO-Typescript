448a1c7e7562852a480fee21e9b2a7c1846a2a04
Shawn <codingshawn@gmail.com>
2021-05-21 16:55:59 +0800

    Implemented frontend display of individual patient info and function to navigate to such page

---
+++ patientorFrontend/src/components/PatientInfo.tsx
@@ -0,0 +1,46 @@
+import axios from "axios";
+import React, { useState } from "react";
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { apiBaseUrl } from "../constants";
+import { Patient } from "../types";
+import { Icon } from "semantic-ui-react";
+
+function PatientInfo() {
+  const [patientData, setPatientData] = useState<Patient | null>(null);
+  const { id } = useParams<{ id: string }>();
+
+  async function getPatientDetails() {
+    try {
+      const response = await axios.get<Patient>(`${apiBaseUrl}/patients/${id}`);
+      setPatientData(response.data);
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+
+  useEffect(() => {
+    void getPatientDetails();
+  }, []);
+
+  if (!patientData) {
+    return null;
+  }
+
+  return (
+    <section>
+      <h2>
+        {patientData.name}{" "}
+        {patientData.gender === "male" ? (
+          <Icon name="mars" />
+        ) : (
+          <Icon name="venus" />
+        )}
+      </h2>
+      <div>ssn: {patientData.ssn}</div>
+      <div>Occupation: {patientData.occupation}</div>
+    </section>
+  );
+}
+
+export default PatientInfo;
