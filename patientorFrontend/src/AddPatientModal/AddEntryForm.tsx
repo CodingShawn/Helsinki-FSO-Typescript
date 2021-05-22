eceff3d10ae19e3759af6e04fd3a686cd2815f0e
Shawn <codingshawn@gmail.com>
2021-05-22 20:31:32 +0800

    Refactored HealthCheck form away and introduced Health Checkup Form button which will display form when clicked

--- patientorFrontend/src/AddPatientModal/AddEntryForm.tsx
+++ patientorFrontend/src/AddPatientModal/AddEntryForm.tsx
@@ -1,15 +1,13 @@
-import { Formik, Field, Form } from "formik";
-import { Button } from "semantic-ui-react";
-import React from "react";
+import HealthCheckForm from "./EntryTypeForms/HealthCheckForm";
+import React, { useState } from "react";
 import { useStateValue } from "../state/state";
-import { TextField } from "./FormField";
 import { NewEntryDetails, Patient } from "../types";
-import { DiagnosisSelection } from "./FormField";
-import { NumberField } from "./FormField";
 import axios from "axios";
 import { apiBaseUrl } from "../constants";
+import { Button } from "semantic-ui-react";
 
 function AddEntryForm({ id }: { id: string }) {
+  const [checkupType, setCheckupType] = useState("");
   const [{ diagnosis }, dispatch] = useStateValue();
 
   async function onSubmit(values: NewEntryDetails) {
@@ -18,7 +16,6 @@ function AddEntryForm({ id }: { id: string }) {
         `${apiBaseUrl}/patients/${id}/entries`,
         values
       );
-      console.log(updatedPatient);
       dispatch({ type: "ADD_ENTRY", payload: updatedPatient });
     } catch (error) {
       console.log(error.response?.data || "Unknown Error");
@@ -26,90 +23,12 @@ function AddEntryForm({ id }: { id: string }) {
   }
 
   return (
-    <Formik
-      initialValues={{
-        description: "",
-        date: "",
-        specialist: "",
-        type: "HealthCheck",
-        healthCheckRating: 0,
-        diagnosisCodes: [],
-      }}
-      onSubmit={onSubmit}
-      validate={(values) => {
-        const requiredError = "Field is required";
-        const errors: { [field: string]: string } = {};
-        
-        if (!values.description) {
-          errors.description = requiredError;
-        }
-        if (!values.date) {
-          errors.date = requiredError;
-        }
-        if (!values.specialist) {
-          errors.specialist = requiredError;
-        }
-        if (values.type !== "HealthCheck") {
-          errors.type = "No such type selected";
-        }
-        if (
-          values.type === "HealthCheck" &&
-          (values.healthCheckRating !== 0 &&
-            values.healthCheckRating !== 1 &&
-            values.healthCheckRating !== 2 &&
-            values.healthCheckRating !== 3)
-        ) {
-          errors.healthCheckRating = "Invalid value for Health Check Rating";
-        }
-        return errors;
-      }}
-    >
-      {function ({ isValid, dirty, setFieldValue, setFieldTouched }) {
-        return (
-          <Form>
-            <Field
-              label="Description"
-              placeholder="Description"
-              name="description"
-              component={TextField}
-            />
-            <Field
-              label="Date"
-              placeholder="Date"
-              name="date"
-              component={TextField}
-            />
-            <Field
-              label="Specialist"
-              placeholder="Specialist"
-              name="specialist"
-              component={TextField}
-            />
-            <Field
-              label="Type"
-              placeholder="Type"
-              name="type"
-              component={TextField}
-            />
-            <Field
-              label="Health Check Rating"
-              name="healthCheckRating"
-              component={NumberField}
-              min={0}
-              max={3}
-            />
-            <DiagnosisSelection
-              setFieldValue={setFieldValue}
-              setFieldTouched={setFieldTouched}
-              diagnoses={Object.values(diagnosis)}
-            />
-            <Button type="submit" color="green" disabled={!dirty || !isValid}>
-              Add Entry
-            </Button>
-          </Form>
-        );
-      }}
-    </Formik>
+    <>
+      <Button onClick={() => setCheckupType("HealthCheck")}>Health Checkup Form</Button>
+      {checkupType === "HealthCheck" && (
+        <HealthCheckForm diagnosis={diagnosis} onSubmit={onSubmit} />
+      )}
+    </>
   );
 }
 
